8743b83171d4d2e3d0a41915aab06a8845c6e677
Arun <arun.sabtharishi@epsilon.com>
2015-06-16 15:02:33 -0500

    Populating with batting and bowling stats

--- src/main/java/com/iscorecard/model/BattingStatsForMatch.java
+++ src/main/java/com/iscorecard/model/BattingStatsForMatch.java
@@ -4,4 +4,10 @@ package com.iscorecard.model;
  * Created by asabtharishi on 6/16/2015.
  */
 public class BattingStatsForMatch {
+    int battingPosition;
+    boolean didNotBat;
+    int ballsFaced;
+    int runsScored;
+    int fours;
+    int sixes;
 }
--- src/main/java/com/iscorecard/model/BowlingStatsForMatch.java
+++ src/main/java/com/iscorecard/model/BowlingStatsForMatch.java
@@ -4,4 +4,8 @@ package com.iscorecard.model;
  * Created by asabtharishi on 6/16/2015.
  */
 public class BowlingStatsForMatch {
+    boolean didNotBowl;
+    int bowlingPosition;
+    int oversBowled;
+    int ballsBowledBeforeCompletingOver;
 }
